5b79b7480d6f932caf766d03a4390fc470563d6b
coddery888 <->
2024-03-04 16:45:54 +0800

    add more attributes

--- src/game/entities/game.entity.ts
+++ src/game/entities/game.entity.ts
@@ -11,9 +11,9 @@ import {
 } from 'typeorm';
 import { User } from 'src/user/entities/user.entity';
 // import { Wallet } from './wallet.entity';
-import { Bet } from './bet.entity';
-import { Claim } from './claim.entity';
-import { BetDto } from '../dto/bet.dto';
+import { Bet } from 'src/bet/entities/bet.entity';
+import { Claim } from '../../claim/entities/claim.entity';
+import { BetDto } from 'src/bet/dto/bet.dto';
 import { DrawResult } from './drawResult.entity';
 
 @Entity()
@@ -23,6 +23,15 @@ export class Game {
 
   @Column({ unique: true })
   epoch: number;
+
+  @Column()
+  maxBetPerNumber: number;
+
+  @Column()
+  closeAt: Date;
+
+  @Column({ default: false })
+  isClosed: boolean;
 
   @OneToMany(() => Bet, (bet) => bet.game)
   bets: Bet[];
