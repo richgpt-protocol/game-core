8e7c2bc2c459fcdb445bef0d15ff167c3f6ece24
coddery888 <->
2024-03-04 16:45:27 +0800

    add more attributes & change prize to nullable(for update prize one by one

--- src/game/entities/drawResult.entity.ts
+++ src/game/entities/drawResult.entity.ts
@@ -11,9 +11,9 @@ import {
 } from 'typeorm';
 import { User } from 'src/user/entities/user.entity';
 // import { Wallet } from './wallet.entity';
-import { Bet } from './bet.entity';
-import { Claim } from './claim.entity';
-import { BetDto } from '../dto/bet.dto';
+import { Bet } from 'src/bet/entities/bet.entity';
+import { Claim } from 'src/claim/entities/claim.entity';
+import { BetDto } from 'src/bet/dto/bet.dto';
 import { Game } from './game.entity';
 
 @Entity()
@@ -22,105 +22,111 @@ export class DrawResult {
   id: number;
 
   @CreateDateColumn()
-  submitAt: Date;
+  fetchStartAt: Date;
 
   @Column()
+  submitBy: string;
+
+  @Column()
+  proof: string;
+
+  @Column({ nullable: true })
   first: string;
 
-  @Column()
+  @Column({ nullable: true })
   second: string;
 
-  @Column()
+  @Column({ nullable: true })
   third: string;
 
-  @Column()
+  @Column({ nullable: true })
   special1: string;
 
-  @Column()
+  @Column({ nullable: true })
   special2: string;
 
-  @Column()
+  @Column({ nullable: true })
   special3: string;
 
-  @Column()
+  @Column({ nullable: true })
   special4: string;
 
-  @Column()
+  @Column({ nullable: true })
   special5: string;
 
-  @Column()
+  @Column({ nullable: true })
   special6: string;
 
-  @Column()
+  @Column({ nullable: true })
   special7: string;
 
-  @Column()
+  @Column({ nullable: true })
   special8: string;
 
-  @Column()
+  @Column({ nullable: true })
   special9: string;
 
-  @Column()
+  @Column({ nullable: true })
   special10: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation1: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation2: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation3: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation4: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation5: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation6: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation7: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation8: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation9: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation10: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation11: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation12: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation13: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation14: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation15: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation16: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation17: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation18: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation19: string;
 
-  @Column()
+  @Column({ nullable: true })
   consolation20: string;
 
   @OneToOne(() => Game, (game) => game.drawResult)
