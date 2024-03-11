ea31de4a8244117308bb1680187941b627b6d3cf
coddery888 <->
2024-03-11 14:43:18 +0800

    remove unused import

--- src/chatbot/chatbot.module.ts
+++ src/chatbot/chatbot.module.ts
@@ -2,23 +2,14 @@ import { Module } from '@nestjs/common';
 import { ChatbotService } from './chatbot.service';
 import { ChatbotController } from './chatbot.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { chatbot } from './entities/chatbot.entity';
-import { AuditLogModule } from 'src/audit-log/audit-log.module';
 import { PermissionModule } from 'src/permission/permission.module';
-import { SharedModule } from 'src/shared/shared.module';
-import { AdminModule } from 'src/admin/admin.module';
-import { SseModule } from 'src/admin/sse/sse.module';
 import { ChatLog } from './entities/chatLog.entity';
 import { Message } from './entities/message.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ChatLog, Message]),
-    // AuditLogModule,
     PermissionModule,
-    // SharedModule,
-    // AdminModule,
-    // SseModule,
   ],
   providers: [ChatbotService],
   controllers: [ChatbotController],
