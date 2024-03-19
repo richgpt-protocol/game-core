b0260da604185ab033016d01a26d5e65fea8bb1c
coddery888 <->
2024-03-19 13:37:56 +0800

    chore: rewind back to main branch codebase

--- src/redeem/redeem.controller.ts
+++ src/redeem/redeem.controller.ts
@@ -1,44 +1,14 @@
-import {
-    BadRequestException,
-    Body,
-    Controller,
-    Get,
-    HttpStatus,
-    Param,
-    Post,
-    Put,
-    Query,
-    Request,
-  } from '@nestjs/common';
-  import { ApiHeader, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
-  import { I18n, I18nContext } from 'nestjs-i18n';
-  import { AdminService } from 'src/admin/admin.service';
-  import { SseService } from 'src/admin/sse/sse.service';
-  import { AuditLogService } from 'src/audit-log/audit-log.service';
-  import { MobileCountries } from 'src/shared/constants/mobile-country.constant';
-  import { HandlerClass } from 'src/shared/decorators/handler-class.decorator';
-  import { IpAddress } from 'src/shared/decorators/ip-address.decorator';
-  import { Secure } from 'src/shared/decorators/secure.decorator';
-  import { UserRole } from 'src/shared/enum/role.enum';
-  import { IHandlerClass } from 'src/shared/interfaces/handler-class.interface';
-  import { SMSService } from 'src/shared/services/sms.service';
-  import { DateUtil } from 'src/shared/utils/date.util';
-  import { RandomUtil } from 'src/shared/utils/random.util';
-  import {
-    ErrorResponseVo,
-    ResponseListVo,
-    ResponseVo,
-  } from 'src/shared/vo/response.vo';
-import { RedeemService } from './redeem.service';
-import { RedeemDto } from './dto/redeem.dto';
-import { GameService } from 'src/game/game.service';
+// import { Body, Controller, HttpStatus, Post, Request } from '@nestjs/common';
+// import { ApiTags } from '@nestjs/swagger';
+// import { Secure } from 'src/shared/decorators/secure.decorator';
+// import { UserRole } from 'src/shared/enum/role.enum';
+// import { RedeemService } from './redeem.service';
+// import { RedeemDto } from './dto/Redeem.dto';
 
-@ApiTags('Redeem')
-@Controller('api/v1/redeem')
-export class RedeemController {
-  constructor(
-    private redeemService: RedeemService,
-  ) {}
+// @ApiTags('Redeem')
+// @Controller('api/v1/redeem')
+// export class RedeemController {
+//   constructor(private redeemService: RedeemService) {}
 
 //   @Secure(null, UserRole.USER)
 //   @Post('redeem')
@@ -58,4 +28,4 @@ export class RedeemController {
 //       };
 //     }
 //   }
-}
+// }
--- src/redeem/redeem.service.ts
+++ src/redeem/redeem.service.ts
@@ -1,41 +1,41 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { RedeemDto } from './dto/redeem.dto';
-import { RedeemTx } from 'src/wallet/entities/redeem-tx.entity';
-import { Repository } from 'typeorm';
-import { UserWallet } from 'src/wallet/entities/user-wallet.entity';
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import { Injectable } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { RedeemDto } from './dto/redeem.dto';
+// import { Redeem } from './entities/redeem.entity';
+// import { Repository } from 'typeorm';
+// import { Wallet } from 'src/wallet/entities/wallet.entity';
 
-@Injectable()
-export class RedeemService {
+// @Injectable()
+// export class RedeemService {
 
-  constructor(
-    @InjectRepository(RedeemTx)
-    private redeemRepository: Repository<RedeemTx>,
-    @InjectRepository(UserWallet)
-    private walletRepository: Repository<UserWallet>,
-  ) {}
+//   constructor(
+//     @InjectRepository(Redeem)
+//     private redeemRepository: Repository<Redeem>,
+//     @InjectRepository(Wallet)
+//     private walletRepository: Repository<Wallet>,
+//   ) {}
 
-  async redeem(id: number, payload: RedeemDto) {
+//   async redeem(id: number, payload: RedeemDto) {
 
-    // TODO: submit redeem to Redeem contract
+//     // TODO: submit redeem to Redeem contract
 
-    const wallet = await this.walletRepository
-      .createQueryBuilder()
-      .where('userId = :id', { id })
-      .getOne();
+//     const wallet = await this.walletRepository
+//       .createQueryBuilder()
+//       .where('userId = :id', { id })
+//       .getOne();
 
-    // const amount = payload.amount;
-    // await this.walletRepository.update(
-    //   wallet.id,
-    //   { redeemable: wallet.redeemable - amount}
-    // )
+//     const amount = payload.amount;
+//     await this.walletRepository.update(
+//       wallet.id,
+//       { redeemable: wallet.redeemable - amount}
+//     )
 
-    // const redeem = this.redeemRepository.create({
-    //   amount,
-    //   wallet
-    // })
-    // const res = await this.redeemRepository.save(redeem);
-    // return res
-  }
-}
+//     const redeem = this.redeemRepository.create({
+//       amount,
+//       wallet
+//     })
+//     const res = await this.redeemRepository.save(redeem);
+//     return res
+//   }
+// }
