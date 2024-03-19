0b3728a12f320d6bc93a42d0c224cf3f52b5503a
coddery888 <->
2024-03-19 12:45:34 +0800

    import needed entities and module, add providers & controllers

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -8,10 +8,28 @@ import { ReloadTx } from './entities/reload-tx.entity';
 import { WalletTx } from './entities/wallet-tx.entity';
 import { GameUsdTx } from './entities/game-usd-tx.entity';
 import { ClaimDetail } from './entities/claim-detail.entity';
+import { WalletController } from './wallet.controller';
+import { ClaimService } from './services/claim.service';
+import { RedeemService } from './services/redeem.service';
+import { BetOrder } from 'src/game/entities/bet-order.entity';
+import { Game } from 'src/game/entities/game.entity';
+import { DrawResult } from 'src/game/entities/draw-result.entity';
+import { PointTx } from 'src/point/entities/point-tx.entity';
+import { Notification } from 'src/notification/entities/notification.entity';
+import { UserNotification } from 'src/notification/entities/user-notification.entity';
+import { Admin } from 'src/admin/entities/admin.entity';
+import { Setting } from 'src/setting/entities/setting.entity';
+import { AdminNotificationService } from 'src/shared/services/admin-notification.service';
+import { PermissionService } from 'src/permission/permission.service';
+import { PermissionModule } from 'src/permission/permission.module';
+import { WalletService } from './wallet.service';
+import { User } from 'src/user/entities/user.entity';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
+      User,
       UserWallet,
       CreditWalletTx,
       ClaimDetail,
@@ -20,10 +38,20 @@ import { ClaimDetail } from './entities/claim-detail.entity';
       GameUsdTx,
       WalletTx,
       ReloadTx,
+      BetOrder,
+      Game,
+      DrawResult,
+      PointTx,
+      Notification,
+      UserNotification,
+      Admin,
+      Setting,
     ]),
+    PermissionModule,
+    SharedModule,
   ],
-  providers: [],
-  controllers: [],
+  providers: [WalletService, ClaimService, RedeemService],
+  controllers: [WalletController],
   exports: [],
 })
 export class WalletlModule {}
