94da7829e0c1cf703b18edec0af79b596cb15922
coddery888 <->
2024-03-21 17:31:39 +0800

    chore: import ReferralTx entity

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -9,10 +9,11 @@ import { SharedModule } from 'src/shared/shared.module';
 import { AdminModule } from 'src/admin/admin.module';
 import { SseModule } from 'src/admin/sse/sse.module';
 import { UserWallet } from 'src/wallet/entities/user-wallet.entity';
+import { ReferralTx } from 'src/referral/entities/referral-tx.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, UserWallet]),
+    TypeOrmModule.forFeature([User, UserWallet, ReferralTx]),
     AuditLogModule,
     PermissionModule,
     SharedModule,
