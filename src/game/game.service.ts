21bf0ea30e067937aa7ea002029f5b70b6e8ab9f
coddery888 <->
2024-03-25 14:55:20 +0800

    fix: revamp set draw result related functions(endpoints replace by fetch/set record directly into db from local bot, setBetClose() run function with nestjs schedule, updateDrawResult() run from game.gateway.emitDrawResult())

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,64 +1,33 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
-import { ChatCompletionMessageParam } from 'openai/resources';
-// import { SendMessageDto } from './dto/bet.dto';
-// import { MongoClient, WithId } from 'mongodb'
-import * as dotenv from 'dotenv';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from 'src/user/entities/user.entity';
-import { BetDto } from 'src/bet/dto/bet.dto';
 import { Game } from './entities/game.entity';
-import { RedeemDto } from '../redeem/dto/redeem.dto';
-import { DrawResultDto } from './dto/drawResult.dto';
 import { DrawResult } from './entities/draw-result.entity';
-import { UserWallet } from 'src/wallet/entities/user-wallet.entity';
-import { RedeemTx } from 'src/wallet/entities/redeem-tx.entity';
 import { BetOrder } from './entities/bet-order.entity';
-import { ClaimDetail } from 'src/wallet/entities/claim-detail.entity';
+import { Cron } from '@nestjs/schedule';
+import { ethers } from 'ethers';
+import { Core__factory, Helper__factory } from 'src/contract';
+import { IHelper, ICore } from 'src/contract/Helper';
+import { WalletTx } from 'src/wallet/entities/wallet-tx.entity';
+import { AdminNotificationService } from 'src/shared/services/admin-notification.service';
+import * as dotenv from 'dotenv';
 dotenv.config();
-
-// const client = new MongoClient('mongodb://localhost:27017')
 
 @Injectable()
 export class GameService {
+  provider = new ethers.JsonRpcProvider(process.env.OPBNB_PROVIDER_RPC_URL);
+
   constructor(
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
-    @InjectRepository(UserWallet)
-    private walletRepository: Repository<UserWallet>,
     @InjectRepository(Game)
     private gameRepository: Repository<Game>,
-    @InjectRepository(BetOrder)
-    private betRepository: Repository<BetOrder>,
-    @InjectRepository(ClaimDetail)
-    private claimRepository: Repository<ClaimDetail>,
-    @InjectRepository(RedeemTx)
-    private redeemRepository: Repository<RedeemTx>,
     @InjectRepository(DrawResult)
     private drawResultRepository: Repository<DrawResult>,
+    @InjectRepository(BetOrder)
+    private betOrderRepository: Repository<BetOrder>,
+    @InjectRepository(WalletTx)
+    private walletTxRepository: Repository<WalletTx>,
+    private adminNotificationService: AdminNotificationService,
   ) {}
-
-  async setDrawResult(id: number, payload: DrawResultDto) {
-    // TODO: submit draw result to Core contract
-
-    const game = await this.gameRepository.findOneBy({
-      epoch: payload.epoch.toString(),
-    });
-    const drawResult = this.drawResultRepository.create({
-      ...payload,
-      game,
-    });
-    const res = await this.drawResultRepository.save(drawResult);
-
-    await this.gameRepository.save(
-      this.gameRepository.create({
-        epoch: game.epoch + 1,
-      }),
-    );
-
-    return res;
-  }
 
   async getDrawResult(epoch: number) {
     const game = await this.gameRepository.findOneBy({
@@ -69,5 +38,159 @@ export class GameService {
       .where({ game })
       .getOne();
     return drawResult;
+  }
+
+  @Cron('0 0 */1 * * *', { utcOffset: 0 }) // every hour UTC time
+  async setBetClose() {
+    try {
+
+      // set bet close in game record for current epoch
+      const game = await this.gameRepository.findOne({
+        where: { isClosed: false },
+      });
+      game.isClosed = true;
+      await this.gameRepository.save(game);
+
+      // submit masked betOrder on-chain
+      const betOrders = await this.betOrderRepository.find({
+        where: {
+          gameId: game.id,
+          isMasked: true,
+        },
+      });
+      // temporarily, private key will fetch shares from mpc server via address and combine
+      const helperBot = new ethers.Wallet(process.env.HELPER_BOT_PRIVATE_KEY, this.provider);
+      const helperContract = Helper__factory.connect(process.env.HELPER_CONTRACT_ADDRESS, helperBot);
+      
+      const userBets: { [key: string]: ICore.BetParamsStruct[] } = {}
+      for (let i = 0; i < betOrders.length; i++) {
+        const betOrder = betOrders[i];
+        const walletTx = await this.walletTxRepository.findOne({
+          where: { id: betOrder.walletTxId },
+          relations: { userWallet: true }
+        })
+        const userAddress = walletTx.userWallet.walletAddress;
+        if (betOrder.bigForecastAmount > 0) {
+          userBets[userAddress].push({
+            epoch: game.epoch,
+            number: betOrder.numberPair,
+            amount: betOrder.bigForecastAmount,
+            forecast: 1, // big
+          });
+        }
+        if (betOrder.smallForecastAmount > 0) {
+          userBets[userAddress].push({
+            epoch: game.epoch,
+            number: betOrder.numberPair,
+            amount: betOrder.smallForecastAmount,
+            forecast: 0, // small
+          });
+        }
+      }
+      const params: IHelper.BetLastMinuteParamsStruct[] = []
+      for (let userAddress in userBets) {
+        params.push({
+          user: userAddress,
+          bets: userBets[userAddress],
+        })
+      }
+      const estimatedGas = await helperContract.betLastMinutes.estimateGas(params);
+      const txResponse = await helperContract.betLastMinutes(params, {
+        gasLimit: estimatedGas * ethers.toBigInt(13) / ethers.toBigInt(10),
+      });
+      const txReceipt = await txResponse.wait();
+
+      if (txReceipt.status === 1) {
+        for (const betOrder of betOrders) {
+          const walletTx = await this.walletTxRepository.findOne({
+            where: { id: betOrder.walletTxId },
+          })
+          walletTx.txHash = txReceipt.hash;
+          walletTx.status = 'S';
+          await this.walletTxRepository.save(walletTx);
+        }
+
+      } else {
+        for (const betOrder of betOrders) {
+          const walletTx = await this.walletTxRepository.findOne({
+            where: { id: betOrder.walletTxId },
+          })
+          walletTx.txHash = txReceipt.hash;
+          await this.walletTxRepository.save(walletTx);
+        }
+
+        throw new Error(`betLastMinutes() on-chain transaction failed, txHash: ${txReceipt.hash}`)
+      }
+
+    } catch (err) {
+      // inform admin
+      await this.adminNotificationService.setAdminNotification(
+        `Error occur in game.service.setBetClose, error: ${err}`,
+        'ExecutionError',
+        'Execution Error',
+        true,
+      );
+    }
+  }
+
+  async updateDrawResult(payload: DrawResult[], gameId: number): Promise<void> {
+    try {
+      // submit draw result to Core contract
+      const setDrawResultBot = new ethers.Wallet(process.env.RESULT_BOT_PRIVATE_KEY, this.provider); // temporarily
+      const coreContract = Core__factory.connect(process.env.CORE_CONTRACT_ADDRESS, setDrawResultBot);
+      const numberPairs = payload.map((result) => result.numberPair);
+      // TODO: set gasLimit
+      const txResponse = await coreContract.setDrawResults(numberPairs, process.env.MAX_BET_AMOUNT, '');
+      const txReceipt = await txResponse.wait();
+
+      // await queryRunner.manager.update(
+      const game = await this.gameRepository.findOneBy({ id: gameId });
+      game.drawTxHash = txReceipt.hash;
+      game.drawTxStatus = 'P';
+      await this.gameRepository.save(game);
+
+      if (txReceipt.status === 1) {
+        game.drawTxStatus = 'S';
+        await this.gameRepository.save(game);
+
+        for (const result of payload) {
+          const betOrders = await this.betOrderRepository.find({
+            where: {
+              gameId,
+              numberPair: result.numberPair,
+            }
+          });
+          for (const betOrder of betOrders) {
+            betOrder.availableClaim = true;
+            await this.betOrderRepository.save(betOrder);
+          }
+        }
+
+      } else {
+        throw new Error(`setDrawResults() on-chain transaction failed, txHash: ${txReceipt.hash}`)
+      }
+
+    } catch (err) {
+      await this.adminNotificationService.setAdminNotification(
+        `Error in game.service.updateDrawResult, error: ${err}`,
+        'executionError',
+        'Execution Error in Game Service',
+        true,
+      );
+
+    }
+  }
+
+  async getAllBets() {
+    try {
+      const game = await this.gameRepository.findOne({
+        where: { isClosed: false },
+        order: { id: 'DESC' },
+      });
+      return { error: null, data: game.betOrders };
+
+    } catch (error) {
+      return { error: error, data: null };
+    }
   }
 }
