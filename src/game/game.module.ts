6fa512e1ebd6b49b58fdec88fa4a6466aa7b73d3
coddery888 <->
2024-03-25 14:44:11 +0800

    feat: add providers notification & gateway

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -10,6 +10,12 @@ import { User } from 'src/user/entities/user.entity';
 import { UserWallet } from 'src/wallet/entities/user-wallet.entity';
 import { RedeemTx } from 'src/wallet/entities/redeem-tx.entity';
 import { ClaimDetail } from 'src/wallet/entities/claim-detail.entity';
+import { WalletTx } from 'src/wallet/entities/wallet-tx.entity';
+import { AdminNotificationService } from 'src/shared/services/admin-notification.service';
+import { Notification } from 'src/notification/entities/notification.entity';
+import { UserNotification } from 'src/notification/entities/user-notification.entity';
+import { Admin } from 'src/admin/entities/admin.entity';
+import { GameGateway } from './game.gateway';
 
 @Module({
   imports: [
@@ -21,10 +27,14 @@ import { ClaimDetail } from 'src/wallet/entities/claim-detail.entity';
       ClaimDetail,
       RedeemTx,
       DrawResult,
+      WalletTx,
+      Notification,
+      UserNotification,
+      Admin,
     ]),
     PermissionModule,
   ],
-  providers: [GameService],
+  providers: [GameService, AdminNotificationService, GameGateway],
   controllers: [GameController],
   exports: [GameService],
 })
