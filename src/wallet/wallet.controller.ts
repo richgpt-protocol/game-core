c75f2aea5b5ac709e654278245b31bceddd25ca1
coddery888 <->
2024-03-28 22:53:42 +0800

    remove unused var & function

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -13,14 +13,9 @@ import { UserRole } from 'src/shared/enum/role.enum';
 import { ErrorResponseVo, ResponseVo } from 'src/shared/vo/response.vo';
 import { WalletService } from './wallet.service';
 import { ClaimService } from './services/claim.service';
-import { GameService } from 'src/game/game.service';
-import { ClaimDto } from './dto/claim.dto';
 import { RedeemService } from './services/redeem.service';
-import { PermissionEnum } from 'src/shared/enum/permission.enum';
-import { PayoutDto } from './dto/payout.dto';
 import { RedeemDto } from 'src/wallet/dto/redeem.dto';
 import { ReviewRedeemDto } from './dto/ReviewRedeem.dto';
-// import { SendMessageDto } from './dto/bet.dto';
 
 @ApiTags('Wallet')
 @Controller('api/v1/wallet')
@@ -214,57 +209,6 @@ export class WalletController {
         message: error.message,
       };
     }
-  }
-
-  @Secure(PermissionEnum.PAYOUT, UserRole.ADMIN)
-  @Post('payout')
-  @ApiHeader({
-    name: 'x-custom-lang',
-    description: 'Custom Language',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'OK',
-    type: ResponseVo,
-  })
-  async payout(
-    @Request() req,
-    @Body() payload: PayoutDto,
-    // @IpAddress() ipAddress,
-    // @HandlerClass() classInfo: IHandlerClass,
-    // @I18n() i18n: I18nContext,
-  ): Promise<ResponseVo<any>> {
-    const payout = await this.redeemService.payout(req.user.userId, payload);
-    return {
-      statusCode: HttpStatus.OK,
-      data: payout,
-      message: '',
-    };
-  }
-
-  @Secure(null, UserRole.ADMIN)
-  @Get('get-pending-payout')
-  @ApiHeader({
-    name: 'x-custom-lang',
-    description: 'Custom Language',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'OK',
-    type: ResponseVo,
-  })
-  async getPendingPayout(
-    @Request() req,
-    // @IpAddress() ipAddress,
-    // @HandlerClass() classInfo: IHandlerClass,
-    // @I18n() i18n: I18nContext,
-  ): Promise<ResponseVo<any>> {
-    const pendingPayout = await this.redeemService.getPendingPayout();
-    return {
-      statusCode: HttpStatus.OK,
-      data: pendingPayout,
-      message: '',
-    };
   }
 
   // TODO
