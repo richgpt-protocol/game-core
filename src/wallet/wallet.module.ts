c3ddf30fcaf3f0ace8ae985c5dd4d431b1fe3ab9
coddery888 <->
2024-03-28 23:07:04 +0800

    chore: remove unused var & commented code

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -19,8 +19,6 @@ import { Notification } from 'src/notification/entities/notification.entity';
 import { UserNotification } from 'src/notification/entities/user-notification.entity';
 import { Admin } from 'src/admin/entities/admin.entity';
 import { Setting } from 'src/setting/entities/setting.entity';
-import { AdminNotificationService } from 'src/shared/services/admin-notification.service';
-import { PermissionService } from 'src/permission/permission.service';
 import { PermissionModule } from 'src/permission/permission.module';
 import { WalletService } from './wallet.service';
 import { User } from 'src/user/entities/user.entity';
@@ -55,31 +53,3 @@ import { SharedModule } from 'src/shared/shared.module';
   exports: [],
 })
 export class WalletlModule {}
-// import { WalletService } from './wallet.service';
-// import { WalletController } from './wallet.controller';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { AuditLogModule } from 'src/audit-log/audit-log.module';
-// import { PermissionModule } from 'src/permission/permission.module';
-// import { SharedModule } from 'src/shared/shared.module';
-// import { AdminModule } from 'src/admin/admin.module';
-// import { SseModule } from 'src/admin/sse/sse.module';
-// import { User } from 'src/user/entities/user.entity';
-// import { Wallet } from './entities/wallet.entity';
-// import { Bet } from 'src/bet/entities/bet.entity';
-// import { GameModule } from 'src/game/game.module';
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forFeature([User, Wallet, Bet]),
-//     // AuditLogModule,
-//     PermissionModule,
-//     // SharedModule,
-//     // AdminModule,
-//     // SseModule,
-//     GameModule
-//   ],
-//   providers: [WalletService],
-//   controllers: [WalletController],
-//   exports: [],
-// })
-// export class WalletModule {}
