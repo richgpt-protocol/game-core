857c0f4f3811192ad61a7cfa5f0b1d917d7f7d7e
coddery888 <->
2024-05-26 11:11:19 +0800

    fix: remove www.test-api.4dgpt.xyz

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(test-api.4dgpt.xyz www.test-api.4dgpt.xyz)
+domains=(test-api.4dgpt.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="siew@daoventures.co" # Adding a valid address is strongly recommended
