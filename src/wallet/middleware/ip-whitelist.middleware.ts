6ebe3cb6d1547f242cf829b3b2c2f50d46f66ad4
coddery888 <->
2024-09-07 12:45:06 +0800

    only allow deposit-bot VM to access /deposit endpoint

---
+++ src/wallet/middleware/ip-whitelist.middleware.ts
@@ -0,0 +1,22 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class IpWhitelistMiddleware implements NestMiddleware {
+  // Define the list of allowed IPs
+  private readonly allowedIps: string[] = ['18.219.125.24'];
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const clientIp = req.ip;
+
+    if (this.allowedIps.includes(clientIp)) {
+      next(); // Allow the request if the IP is in the list
+    } else {
+      throw new UnauthorizedException('IP not allowed');
+    }
+  }
+}
