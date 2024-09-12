8b049790fc1a04910d33ba08f4daa8dea3cb2f1d
coddery888 <->
2024-09-12 14:59:48 +0800

    comment out BullModule for testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,9 +30,9 @@ import { ChatbotModule } from './chatbot/chatbot.module';
 import { BackOfficeModule } from './back-office/back-office.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PointModule } from './point/point.module';
-import { BullModule } from '@nestjs/bullmq';
+// import { BullModule } from '@nestjs/bullmq';
 import { QueueModule } from './queue/queue.module';
-import { QueueOptions } from 'bullmq';
+// import { QueueOptions } from 'bullmq';
 
 @Module({
   imports: [
@@ -61,17 +61,23 @@ import { QueueOptions } from 'bullmq';
       }),
       inject: [ConfigService],
     }),
-    BullModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) =>
-        ({
-          connection: {
-            host: configService.get('REDIS_HOST'),
-            port: +configService.get('REDIS_PORT'),
-          },
-        }) as QueueOptions,
-      inject: [ConfigService],
-    }),
+    // BullModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: async (configService: ConfigService) =>
+    //     ({
+    //       connection: {
+    //         host: configService.get('REDIS_HOST'),
+    //         port: +configService.get('REDIS_PORT'),
+    //       },
+    //     }) as QueueOptions,
+    //   inject: [ConfigService],
+    // }),
+    // BullModule.forRoot({
+    //   connection: {
+    //     host: 'redis',
+    //     port: 6380,
+    //   },
+    // }),
     UserModule,
     SettingModule,
     I18nModule.forRoot({
