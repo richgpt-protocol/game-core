07c14015fab0effc23deb56c3b02b2529047631d
devRichGpt <dev1RichGPT@proton.me>
2024-10-15 14:11:14 +0530

    ignore specific referral-codes

--- src/database/seeds/setting.seed.ts
+++ src/database/seeds/setting.seed.ts
@@ -137,6 +137,10 @@ export default class CreateSettings implements Seeder {
           key: SettingEnum.CREDIT_EXPIRY_DAYS,
           value: 90,
         },
+        {
+          key: SettingEnum.FILTERED_REFERRAL_CODES,
+          value: JSON.stringify([]),
+        },
       ])
       .execute();
   }
