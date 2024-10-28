b60dcc695fb9600e4eb343a788573d7dc299253a
cassriogpt <dev.richgpt@gmail.com>
2024-10-28 19:51:11 +0800

    Update

--- src/migrations/1730116239954-UpdateTxNote.ts
+++ src/migrations/1730116239954-UpdateTxNote.ts
@@ -1,10 +1,9 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class UpdateTxNote1730113408521 implements MigrationInterface {
-    name = 'UpdateTxNote1730113408521'
+export class UpdateTxNote1730116239954 implements MigrationInterface {
+    name = 'UpdateTxNote1730116239954'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`deposit_tx\` DROP COLUMN \`note\``);
         await queryRunner.query(`ALTER TABLE \`credit_wallet_tx\` ADD \`note\` varchar(255) NULL`);
         await queryRunner.query(`ALTER TABLE \`wallet_tx\` ADD \`note\` varchar(255) NULL`);
     }
@@ -12,7 +11,6 @@ export class UpdateTxNote1730113408521 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`wallet_tx\` DROP COLUMN \`note\``);
         await queryRunner.query(`ALTER TABLE \`credit_wallet_tx\` DROP COLUMN \`note\``);
-        await queryRunner.query(`ALTER TABLE \`deposit_tx\` ADD \`note\` varchar(255) NULL COMMENT 'Note from admin when deposit is approved/rejected by admin'`);
     }
 
 }
