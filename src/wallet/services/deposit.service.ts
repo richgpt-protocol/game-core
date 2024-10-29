8696332ddd006c2131dd04c537364c3fc2fab5ef
devRichGpt <dev1RichGPT@proton.me>
2024-10-29 22:07:48 +0530

    commit queryrunner  before adding it to next queue

--- src/wallet/services/deposit.service.ts
+++ src/wallet/services/deposit.service.ts
@@ -122,7 +122,9 @@ export class DepositService implements OnModuleInit {
         },
       });
       if (!userWallet) {
-        throw new BadRequestException(`UserWallet for walletAddress ${payload.walletAddress} not found`);
+        throw new BadRequestException(
+          `UserWallet for walletAddress ${payload.walletAddress} not found`,
+        );
       }
 
       const user = await queryRunner.manager.findOne(User, {
@@ -131,9 +133,11 @@ export class DepositService implements OnModuleInit {
         },
       });
       if (!user) {
-        throw new BadRequestException(`User for walletId ${userWallet.id} not found`);
+        throw new BadRequestException(
+          `User for walletId ${userWallet.id} not found`,
+        );
       }
-      
+
       const miniGameUSDTSenderSetting = await queryRunner.manager.findOne(
         Setting,
         {
@@ -265,7 +269,9 @@ export class DepositService implements OnModuleInit {
           relations: ['walletTx'],
         });
         if (!usdtTx) {
-          throw new BadRequestException(`usdt_tx for id ${payload.usdtTxId} not found`);
+          throw new BadRequestException(
+            `usdt_tx for id ${payload.usdtTxId} not found`,
+          );
         }
 
         const gameTx = await queryRunner.manager.findOne(GameTx, {
@@ -274,7 +280,9 @@ export class DepositService implements OnModuleInit {
           },
         });
         if (!gameTx) {
-          throw new BadRequestException(`game_tx for usdt_tx.id ${payload.usdtTxId} not found`);
+          throw new BadRequestException(
+            `game_tx for usdt_tx.id ${payload.usdtTxId} not found`,
+          );
         }
 
         //Already have a walletTx if the txType is CAMPAIGN
@@ -561,6 +569,8 @@ export class DepositService implements OnModuleInit {
         gameUsdTx.walletTxs = [depositTx.walletTx];
         await queryRunner.manager.save(gameUsdTx);
 
+        await queryRunner.commitTransaction();
+
         await this.queueService.addJob(
           QueueName.DEPOSIT,
           `gameusd-${gameUsdTx.id}`,
@@ -576,8 +586,6 @@ export class DepositService implements OnModuleInit {
           `Escrow transaction failed with hash: ${onchainEscrowTxHash}`,
         );
       }
-
-      await queryRunner.commitTransaction();
     } catch (error) {
       this.logger.error('handleEscrowTx() error:', error);
       throw new Error(`Error processing deposit ${error}`);
