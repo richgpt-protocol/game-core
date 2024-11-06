95740b690bd6948698e63fc2dc1bca49ea5beb80
cassriogpt <dev.richgpt@gmail.com>
2024-11-06 18:14:43 +0800

    Update to the live environment

--- src/migrations/1730888036886-updateBettingWallet.ts
+++ src/migrations/1730888036886-updateBettingWallet.ts
@@ -1,12 +1,14 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class BettingUpgrade1729846863111 implements MigrationInterface {
-    name = 'BettingUpgrade1729846863111'
+export class UpdateBettingWallet1730888036886 implements MigrationInterface {
+    name = 'UpdateBettingWallet1730888036886'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`FK_488a7566d46ce7af10794e2c66a\` ON \`game_usd_tx\``);
+        await queryRunner.query(`ALTER TABLE \`game_usd_tx\` DROP FOREIGN KEY \`FK_488a7566d46ce7af10794e2c66a\``);
+        await queryRunner.query(`ALTER TABLE \`game_usd_tx\` CHANGE \`creditWalletTxId\` \`maskingTxHash\` int NULL`);
+        await queryRunner.query(`ALTER TABLE \`credit_wallet_tx\` ADD \`note\` varchar(255) NULL`);
         await queryRunner.query(`ALTER TABLE \`credit_wallet_tx\` ADD \`gameUsdTxId\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`deposit_tx\` ADD \`note\` varchar(255) NULL COMMENT 'Note from admin when deposit is approved/rejected by admin'`);
+        await queryRunner.query(`ALTER TABLE \`wallet_tx\` ADD \`note\` varchar(255) NULL`);
         await queryRunner.query(`ALTER TABLE \`bet_order\` DROP FOREIGN KEY \`FK_a8375df8a485f65eec63f5a8a81\``);
         await queryRunner.query(`ALTER TABLE \`bet_order\` CHANGE \`walletTxId\` \`walletTxId\` int NULL`);
         await queryRunner.query(`ALTER TABLE \`game_usd_tx\` DROP COLUMN \`maskingTxHash\``);
@@ -22,9 +24,11 @@ export class BettingUpgrade1729846863111 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`game_usd_tx\` ADD \`maskingTxHash\` int NULL`);
         await queryRunner.query(`ALTER TABLE \`bet_order\` CHANGE \`walletTxId\` \`walletTxId\` int NOT NULL`);
         await queryRunner.query(`ALTER TABLE \`bet_order\` ADD CONSTRAINT \`FK_a8375df8a485f65eec63f5a8a81\` FOREIGN KEY (\`walletTxId\`) REFERENCES \`wallet_tx\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`deposit_tx\` DROP COLUMN \`note\``);
+        await queryRunner.query(`ALTER TABLE \`wallet_tx\` DROP COLUMN \`note\``);
         await queryRunner.query(`ALTER TABLE \`credit_wallet_tx\` DROP COLUMN \`gameUsdTxId\``);
-        await queryRunner.query(`CREATE INDEX \`FK_488a7566d46ce7af10794e2c66a\` ON \`game_usd_tx\` (\`maskingTxHash\`)`);
+        await queryRunner.query(`ALTER TABLE \`credit_wallet_tx\` DROP COLUMN \`note\``);
+        await queryRunner.query(`ALTER TABLE \`game_usd_tx\` CHANGE \`maskingTxHash\` \`creditWalletTxId\` int NULL`);
+        await queryRunner.query(`ALTER TABLE \`game_usd_tx\` ADD CONSTRAINT \`FK_488a7566d46ce7af10794e2c66a\` FOREIGN KEY (\`creditWalletTxId\`) REFERENCES \`credit_wallet_tx\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
 }
