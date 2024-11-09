0502fb2c4db98ec93d0f083c944bd3a753a394a6
cassriogpt <dev.richgpt@gmail.com>
2024-11-09 16:57:34 +0800

    Added dummy data for testing

--- src/shared/constants/topAccountTestnet.constant.ts
+++ src/shared/constants/topAccountTestnet.constant.ts
@@ -3,16 +3,21 @@ export const TopAccountTestnet: Array<{
   accountValue: string;
   pointAmount: number;
 }> = [
-  // {
-  //   signUpMethod: 'PhoneNumber',
-  //   accountValue: '+60132066680',
-  //   pointAmount: 22030,
-  // },
-  // {
-  //   signUpMethod: 'TG',
-  //   accountValue: '6722889565',
-  //   pointAmount: 32000,
-  // },
+  {
+    signUpMethod: 'PhoneNumber',
+    accountValue: '+60132066680',
+    pointAmount: 22030,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '6556189043',
+    pointAmount: 32000,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '6971444810',
+    pointAmount: 48000,
+  },
 ];
 
 export const TESTNET_CAMPAIGN_ID = 1;
