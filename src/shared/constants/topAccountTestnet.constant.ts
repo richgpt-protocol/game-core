716b3396d300c6b1b34b838433abcae8d9c1e1eb
cassriogpt <dev.richgpt@gmail.com>
2024-11-10 00:52:25 +0800

    Added whitelist

--- src/shared/constants/topAccountTestnet.constant.ts
+++ src/shared/constants/topAccountTestnet.constant.ts
@@ -3,16 +3,56 @@ export const TopAccountTestnet: Array<{
   accountValue: string;
   pointAmount: number;
 }> = [
-  // {
-  //   signUpMethod: 'PhoneNumber',
-  //   accountValue: '+60132066680',
-  //   pointAmount: 22030,
-  // },
-  // {
-  //   signUpMethod: 'TG',
-  //   accountValue: '6722889565',
-  //   pointAmount: 32000,
-  // },
+  {
+    signUpMethod: 'TG',
+    accountValue: '5208724239',
+    pointAmount: 85295,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '1940119344',
+    pointAmount: 78793,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '2107294221',
+    pointAmount: 77879,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '1901583779',
+    pointAmount: 74994,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '5556140861',
+    pointAmount: 67490,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '755034223',
+    pointAmount: 52888,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '6795885706',
+    pointAmount: 48457,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '6556189043',
+    pointAmount: 48000,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '704696746',
+    pointAmount: 33779,
+  },
+  {
+    signUpMethod: 'TG',
+    accountValue: '653879398',
+    pointAmount: 17626,
+  },
 ];
 
 export const TESTNET_CAMPAIGN_ID = 2;
