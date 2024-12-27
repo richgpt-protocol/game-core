7ef1eafa49c10329c794f99d38f9fbff6f50c07f
coddery888 <->
2024-12-27 11:02:29 +0800

    use only hashedSeedChar, add participantIsUpdated to determine if participant is updated, remove prefix 0x for consistency when verify, add projectName for query purpose

--- src/database/seeds/squidGameStage2.seed.ts
+++ src/database/seeds/squidGameStage2.seed.ts
@@ -1,25 +1,21 @@
-import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+import { Seeder } from 'typeorm-extension';
 import { ethers } from 'ethers';
 import { DataSource } from 'typeorm';
 
-const seedChar: string = '9';
-const seedString: string = `The seed character for FUYO X SQUID GAME - STAGE 2 is: ${seedChar}`;
+// please use lowercase if seedChar is not number
+const seedChar: string = 'a';
 
-const projectName = 'FUYO X SQUID GAME - STAGE 2'; // to fetch startTime & endTime
+// must match with the projectName in jackpot table
+const projectName = 'FUYO X SQUID GAME - STAGE 2';
 
 export type SQUID_GAME_STAGE_2 = {
+  projectName: string;
   seedChar: string;
-  seedString: string;
-  hashedSeedString: string;
+  hashedSeedChar: string;
   startTime: Date;
   endTime: Date;
+  participantIsUpdated: boolean;
 };
-
-export enum SQUID_GAME_PARTICIPANT_STATUS {
-  STAGE_2_SUCCESS = 'STAGE_2_SUCCESS',
-  STAGE_2_FAILED = 'STAGE_2_FAILED',
-  STAGE_2_ALREADY_SUCCESS = 'STAGE_2_ALREADY_SUCCESS',
-}
 
 export default class SquidGameStage2 implements Seeder {
   /**
@@ -29,10 +25,7 @@ export default class SquidGameStage2 implements Seeder {
    */
   track = false;
 
-  public async run(
-    dataSource: DataSource,
-    factoryManager: SeederFactoryManager,
-  ): Promise<void> {
+  public async run(dataSource: DataSource): Promise<void> {
     const project = await dataSource
       .createQueryBuilder()
       .select('jackpot')
@@ -43,8 +36,8 @@ export default class SquidGameStage2 implements Seeder {
       throw new Error('Project not found');
     }
 
-    // computes UTF-8 bytes of seedString and computes the keccak256
-    const hashedSeedString = ethers.id(seedString);
+    // computes UTF-8 bytes of seedString, computes the keccak256 and remove the 0x prefix
+    const hashedSeedChar = ethers.id(seedChar).slice(2);
 
     // insert or update into setting table
     const result = await dataSource
@@ -55,11 +48,12 @@ export default class SquidGameStage2 implements Seeder {
         {
           key: 'ENABLE_SQUID_GAME_STAGE_2',
           value: JSON.stringify({
+            projectName,
             seedChar,
-            seedString,
-            hashedSeedString,
+            hashedSeedChar,
             startTime: project.startTime,
             endTime: project.endTime,
+            participantIsUpdated: false,
           } as SQUID_GAME_STAGE_2),
         },
       ])
@@ -67,11 +61,11 @@ export default class SquidGameStage2 implements Seeder {
       .execute();
     console.log(result);
 
-    // log the hashedSeedString
-    console.log('Hashed seed string:', hashedSeedString);
+    // log the hashedSeedChar
+    console.log('Hashed seed char:', hashedSeedChar);
   }
 }
-// how to verify the hashed seed string?
-// after announce the seed character, verify the hashed seed string with:
-// ethers.id('The seed character for FUYO X SQUID GAME - STAGE 2 is: <seed character>')
-// the result should be exactly same as the hashed seed string
+// how to verify the hashed seed character?
+// after announce the seed character, verify the hashed seed character with:
+// https://emn178.github.io/online-tools/keccak_256.html
+// input the seed character should get exactly same hashed seed character
