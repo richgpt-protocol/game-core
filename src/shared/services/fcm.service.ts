8f1c44be45e7894504834fc982e45801c0486478
cassriogpt <dev.richgpt@gmail.com>
2025-01-24 15:46:45 +0800

    Change the repository code from query runner

--- src/shared/services/fcm.service.ts
+++ src/shared/services/fcm.service.ts
@@ -57,8 +57,7 @@ export class FCMService {
     message: string,
   ) {
     try {
-      const queryRunner = this.dataSource.createQueryRunner();
-      const user = await queryRunner.manager.findOne(User, {
+      const user = await this.userRepository.findOne({
         where: {
           id: userId,
         },
