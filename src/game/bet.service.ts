e1b702ae599de51fd0c6a4795a6bf46c8b7e4e6f
coddery888 <->
2025-01-25 10:56:10 +0800

    chore: remove unused squid game participant query

--- src/game/bet.service.ts
+++ src/game/bet.service.ts
@@ -1525,18 +1525,7 @@ export class BetService implements OnModuleInit {
           currentTime: currentTime,
         })
         .getOne();
-
-      const participant = await queryRunner.manager
-        .createQueryBuilder(SquidGameParticipant, 'participant')
-        .where('participant.userId = :userId', {
-          userId: userWallet.user.id,
-        })
-        .getOne();
-
-      // check if the user is eligible to participate in the jackpot
-      if (!jackpot) {
-        return;
-      }
+      if (!jackpot) return;
 
       // check if any numberPair in betOrders is greater than minimumBetAmount
       for (const betOrder of betOrders) {
